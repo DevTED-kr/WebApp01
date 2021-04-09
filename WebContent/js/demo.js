e50ee209715711948b862612ba79e2a37f4ed7f7
DevTED-kr <keting2@naver.com>
2021-04-09 23:41:56 +0900

    import sync function requestAnimationFrame

--- WebContent/js/demo.js
+++ WebContent/js/demo.js
@@ -4,5 +4,50 @@
  *
  */
 
-console.log("demo.js");
+
+(function($){
+	
+
+	console.log("demo.js");	
+	
+	$('.btn').click(function(){
+		// javascript
+		console.log(this);
+		 
+		// jQuery
+		console.log($(this));
+	});
+	
+	
+	/**
+	 * Import below open source. 
+	 * @auth: DevTED-kr
+	 * @see: http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	 * @see: http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	 * @license: MIT license
+	 */
+ 	var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+      window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+      window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                 || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+      window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+          timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+      };
+
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+      };
+	
+}(jQuery));
 
