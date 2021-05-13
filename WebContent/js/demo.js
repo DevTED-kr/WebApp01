b85271ea1050d256e180236c8674b4a725b4c3a7
DevTED-kr <keting2@naver.com>
2021-05-13 22:14:58 +0900

    Add font awesome and Style

--- WebContent/js/demo.js
+++ WebContent/js/demo.js
@@ -5,34 +5,34 @@
  */
 
 // This can be used to set the Particles Effects. Check README for more details!
-//document.addEventListener('DOMContentLoaded', function () {
-//	console.log("addEventlistener");
-//  particleground(document.getElementById('particles'), {
-//    dotColor: '#5cbdaa',
-//    lineColor: '#5cbdaa'
-//  });
-//  var intro = document.getElementById('intro');
-//  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-//}, false);
+document.addEventListener('DOMContentLoaded', function () {
+	console.log("addEventlistener");
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
 
 
 
-(function($){
-	
-	document.addEventListener('DOMContentLoaded',function(){
-		console.log("demo2.js");	
-	});
-
-	console.log("demo.js");	
-	 
-	$('.btn').click(function(){
-		// javascript
-		console.log(this);
-		 
-		// jQuery
-		console.log($(this));
-	});
-	
-
-}(jQuery));
+//(function($){
+//	
+//	document.addEventListener('DOMContentLoaded',function(){
+//		console.log("demo2.js");	
+//	});
+//
+//	console.log("demo.js");	
+//	 
+//	$('.btn').click(function(){
+//		// javascript
+//		console.log(this);
+//		 
+//		// jQuery
+//		console.log($(this));
+//	});
+//	
+//
+//}(jQuery));
 
